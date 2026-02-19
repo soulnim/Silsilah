66f3daa00e735f6cfe4c2c74c49384ff3cdb31f5
Soulnim <soulnimorigin@gmail.com>
2026-02-20 04:02:42 +0800

    fix url issue for mobile installation

--- silsilah-sw.js
+++ silsilah-sw.js
@@ -1,7 +1,7 @@
 const CACHE = 'silsilah-v1';
 const ASSETS = [
- './index.html',
- './silsilah.webmanifest',
+ '/Silsilah/index.html',
+ '/Silsilah/silsilah.webmanifest',
  'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;700&family=Lato:wght@300;400;700&display=swap'
 ];
 
